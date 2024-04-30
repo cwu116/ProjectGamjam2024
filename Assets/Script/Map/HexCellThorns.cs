85aa817eeedebcd968adbabd859f4737167fac6a
AitenJay <1481110164@qq.com>
2024-04-30 15:19:08 +0800

    制作地形环境预制体，并添加对应组件

---
+++ Assets/Script/Map/HexCellThorns.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexCellThorns : HexCell
+{
+    public HexCellThorns()
+    {
+        Type = HexType.Thorns;
+    }
+}
