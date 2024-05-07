8c53dca73c485d14960a7cde479570130c2c7675
Fgter <1651794787@qq.com>
2024-05-07 20:04:28 +0800

    Auto stash before merge of "MapSystem" and "origin/MapSystem"
    1

--- Assets/Script/UI/UIMain/UIPotion.cs
+++ Assets/Script/UI/UIMain/UIPotion.cs
@@ -19,15 +19,114 @@ namespace Game.UI
 
         public void Init(Item_Data potion,UIMain owner)
         {
-            
-            List<Sprite> sprites = new List<Sprite>();
-            //sprites.Add(Resources.Load<Sprite>(UIImagePath.ImagePath + "ºì"));
-            sprites.Add(Resources.Load<Sprite>(UIImagePath.ImagePath + "ÂÌ"));
-            //sprites.Add(Resources.Load<Sprite>(UIImagePath.ImagePath + "À¶"));
-            //sprites.Add(Resources.Load<Sprite>(UIImagePath.ImagePath + "·Û"));
-            //sprites.Add(Resources.Load<Sprite>(UIImagePath.ImagePath + "×Ï"));
-            icon.sprite = sprites[Random.Range(0, sprites.Count)];
 
+            Sprite sprite;
+            if(potion.Name.Equals("±Þ×Ó"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "ºì");
+            }
+            else if(potion.Name.Equals("ÈâÌå¸ÄÔì"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "ºì");
+            }
+            else if (potion.Name.Equals("Á¦Á¿"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "ºì");
+            }
+            else if (potion.Name.Equals("ÓÄ»ê"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "ºì");
+            }
+            else if (potion.Name.Equals("ÐË·Ü¶¾Ò©"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "·Û");
+            }
+            else if (potion.Name.Equals("ÎåÊ®²½Éß"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "·Û");
+            }
+            else if (potion.Name.Equals("¶ñÄ§ë·Áî"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "·Û");
+            }
+            else if (potion.Name.Equals("¶ñÄ§Á¦Á¿"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "·Û");
+            }
+            else if (potion.Name.Equals("»î°Ð×Ó"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "³È");
+            }
+            else if (potion.Name.Equals("»î°Ð×Ó"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "³È");
+            }
+            else if (potion.Name.Equals("¶¬Ãß"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "³È");
+            }
+            else if (potion.Name.Equals("ÒþÈë³¾ÑÌ"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "³È");
+            }
+            else if (potion.Name.Equals("Ó¼"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "³È");
+            }
+            else if (potion.Name.Equals("ÑªÈâ×£¸£"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "×Ï");
+            }
+            else if (potion.Name.Equals("È¼ÉÕÆ¿"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "×Ï");
+            }
+            else if (potion.Name.Equals("ÖÏÏ¢"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "×Ï");
+            }
+            else if (potion.Name.Equals("ºÚ°µÖ®ÐÄ"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "×Ï");
+            }
+            else if (potion.Name.Equals("ÅòÕÍ"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "À¶");
+            }
+            else if (potion.Name.Equals("»ðÑæ¾»»¯"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "À¶");
+            }
+            else if (potion.Name.Equals("²£Á§´óÅÚ"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "À¶");
+            }
+            else if (potion.Name.Equals("³¤±Ûºï"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "À¶");
+            }
+            else if (potion.Name.Equals("ÊµÑéÌå1ºÅ"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "ÂÌ");
+            }
+            else if (potion.Name.Equals("ÊµÑéÌå2ºÅ"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "ÂÌ");
+            }
+            else if (potion.Name.Equals("ÊµÑéÌå3ºÅ"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "ÂÌ");
+            }
+            else if (potion.Name.Equals("Çà²ÝÒ©¼Á"))
+            {
+                sprite = Resources.Load<Sprite>(UIImagePath.ImagePath + "ÂÌ");
+            }
+            else
+            {
+                sprite= Resources.Load<Sprite>(UIImagePath.ImagePath + "ÂÌ");
+            }
+
+            this.icon.sprite = sprite;
             this.potion = potion;
             this.owner = owner;
         }
